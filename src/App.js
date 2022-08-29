7dc7987aa89197161b11cbaa2d4ca09f9e0ea575
RutujaRM <96306705+RutujaRM@users.noreply.github.com>
2022-08-29 11:04:35 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import "./Styles/Tailwind_prebuild.css"; //Here we import tailwind CSS
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className=""> Wheather APP</h1>
+    </div>
+  );
+}
